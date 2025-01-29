b1b71c8b17deae09a12ff0a6f60e90af4ffb2f18
geekyparth2004 <goelparth20049@gmail.com>
2025-01-29 22:37:07 +0530

    Changes

---
+++ Day-6/02.437.path-sum-iii.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=437 lang=cpp
+ *
+ * [437] Path Sum III
+ *
+ * https://leetcode.com/problems/path-sum-iii/description/
+ *
+ * algorithms
+ * Medium (46.08%)
+ * Likes:    11307
+ * Dislikes: 540
+ * Total Accepted:    651K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[10,5,-3,3,2,null,11,3,-2,null,1]\n8'
+ *
+ * Given the root of a binary tree and an integer targetSum, return the number
+ * of paths where the sum of the values along the path equals targetSum.
+ * 
+ * The path does not need to start or end at the root or a leaf, but it must go
+ * downwards (i.e., traveling only from parent nodes to child nodes).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+ * Output: 3
+ * Explanation: The paths that sum to 8 are shown.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * Output: 3
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 1000].
+ * -10^9 <= Node.val <= 10^9
+ * -1000 <= targetSum <= 1000
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        
+    }
+};
+// @lc code=end
+
